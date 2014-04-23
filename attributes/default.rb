c8e8d74f6855060914e542d122926bbe53160c76
Fernando Honig <fernando.honig@rackspace.co.uk>
2014-04-23 14:17:59 +0000

    .

--- attributes/default.rb
+++ attributes/default.rb
@@ -3,7 +3,7 @@ default['rackspace_htpasswd']['install_method'] = 'packages' # Can be python or 
 default['rackspace_htpasswd']['built-in']['lang'] = 'python'
 
 default['rackspace_htpasswd']['install_dir'] = '/usr/local/bin'
-default['rackspace_htpasswd']['path']        = ::File.join(node['htpasswd']['install_dir'], 'htpasswd')
+default['rackspace_htpasswd']['path']        = ::File.join(node['rackspace_htpasswd']['install_dir'], 'htpasswd')
 
 default['rackspace_htpasswd']['packages'] = value_for_platform_family(
   ['rhel', 'fedora', 'suse'] => ['httpd-tools'],
